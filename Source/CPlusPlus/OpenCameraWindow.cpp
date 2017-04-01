08675cb233f5c57ec7258e608d35e58e502a3434
Ace <zhangkaiduo@foxmail.com>
2017-04-01 12:41:12 +0800

    first commit

---
+++ Source/CPlusPlus/OpenCameraWindow.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+//本函数用于调用摄像头，并开启一个窗口实时显示摄像头画面
+int main(int ArgCount, char** ArgVector)//ArgCount代表调用此函数时输入参数的个数;ArgVector是输入参数字符串数组的头指针
+{
+			
+
+
+}
