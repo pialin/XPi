90732aac36cb6d5bd98226afbce5468705e3d17c
Ace <zhangkaiduo@foxmail.com>
2017-04-05 12:54:18 +0800

    add put text and frame capture

--- Source/CPlusPlus/OpenCameraWindow.cpp
+++ Source/CPlusPlus/OpenCameraWindow.cpp
@@ -33,20 +33,25 @@ int main(int NumArg, char** ArgVector)				//本函数用于调用摄像头，并
 
     std::cout << "Frame size: " << FrameWidth << "*" << FrameHeight << std::endl;
                                                                 //显示帧分辨率 
-    std::cout << "Frames per second: " << FrameGapTime << std::endl;
+    std::cout << "Frames per second: " << CameraFps << std::endl;
                                                                 //显示帧率
-                                                                                    
+
+    //const cv::Rect ButtonArea = cv::Rect(
+    //                                    FrameWidth*0.8,FrameHeight*0.8，
+    //                                    FrameWidth*0.1,FrameHeight*0.1
+    //                                );                          //设置按钮的位置和尺寸                                                                                
     cv::Mat CameraFrame;                                        //用于作为显示图像帧的暂存矩阵
     std::string CurrentTime;                                     //用于存储当前时间(1970年1月1日至今经过的秒数) time_t为time.h中定义的long类型数据
     std::string CapturedFrameFileName;                          //保存帧图像的文件名   
-    char PressedKeyAscii;                                       //用于存储按下按键的Ascii码
+    signed char PressedKeyAscii;                                       //用于存储按下按键的Ascii码
     std::string ResultText;                                     //用于保存tensorflow程序输出的物体类别
     while(1)
     {
 	VideoCaptureInstance >> CameraFrame;
 
 	if(CameraFrame.empty()) break;				//判断是否成功取到图像帧,如果无法取到帧则跳出循环，退出函数
-		
+	
+        //CameraFrame(ButtonArea) = CameraFrame(ButtonArea) *
 	cv::imshow("Camera",CameraFrame);                       //显示捕获得到的图像帧
 
         PressedKeyAscii = cv::waitKey(FrameGapTime);            //waitKey函数等待键盘按键按下，所给参数为等待的时长（单位：毫秒）,
@@ -54,25 +59,28 @@ int main(int NumArg, char** ArgVector)				//本函数用于调用摄像头，并
 
 	if(PressedKeyAscii == 32) 	                        //在此判断返回值大于等于0目的是对任意键响应，即按任意键结束摄像头
         {   
-            CurrentTime = std::to_string(time(NULL));                           //获取当前系统时间
+            CurrentTime = std::to_string(time(NULL));           //获取当前系统时间
             CapturedFrameFileName = "../CapturedFrame/" + CurrentTime ;
             CapturedFrameFileName += ".jpg" ;
                                                                 //获取帧图像的保存文件名   
             cv::imwrite(CapturedFrameFileName,CameraFrame);     //保存帧图像
-            //cv::waitKey(3000); 
-            ResultText = "Hello \n OpenCV!";                    //TODO 加上tensorflow代码
+            cv::waitKey(3000); 
+            ResultText = "Hello OpenCV!";                    //TODO 加上tensorflow代码
 
             cv::putText                                         //在帧图像上绘制文本 
             (                                           
                 CameraFrame,                                    //所要绘制的对象
                 ResultText,                                     //文本内容
-                cv::Point(0,0),                                 //绘制起始点
+                cv::Point(0,30),                                 //绘制起始点
                 cv::FONT_HERSHEY_COMPLEX,                       //字体
                 1,                                              //文字缩放系数
-                cv::Scalar(255,255,255),                        //文字颜色
+                cv::Scalar(255,255,255),                              //文字颜色
                 1,                                              //文字线宽
                 cv::LINE_AA                                     //文字线型
             );
+
+            cv::imshow("Camera",CameraFrame);                   //绘制文字后更新窗口显示内容  
+
             PressedKeyAscii = cv::waitKey(0);                   //无限期等待按键
             if(PressedKeyAscii == 32)  continue;                //如果按下的是空格键，则继续采集和显示图像
             else if(PressedKeyAscii >= 0) break;                //如果按下的是除空格键外的其他按键，则结束程序
